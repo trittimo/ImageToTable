fbbacdfd305f1fcdebbce0b56d208de8f64e9baa
Michael Trittin <trittimo@rose-hulman.edu>
2018-01-29 12:10:06 -0500

    First commit

---
+++ image-to-table.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import sys
+import os
+
+def convert(image, outpath):
+    with open(outpath, "w") as outfile:
+        outfile.write("<html><body><table name=\"image\"><tr>")
+
+        pixels = image.getdata()
+        width = image.width
+        index = 1
+        needswrite = False
+        for color in pixels:
+            if needswrite:
+                outfile.write("<tr style=\"height: 10px; width: 10px;\">")
+                needswrite = False
+            outfile.write("<td style=\"height: 10px; width: 10px; background-color: #%02x%02x%02x;\"></td>" % (color[0], color[1], color[2]))
+            if index % width == 0:
+                outfile.write("</tr>")
+                needswrite = True
+            index += 1
+        outfile.write("</table></body></html>")
+
+
+if __name__ == '__main__':
+    for infile in sys.argv[1:]:
+        file, extension = os.path.splitext(infile)
+        outfile = file + ".html"
+        if infile != outfile:
+            try:
+                convert(Image.open(infile), outfile)
+            except IOError:
+                print("Unable to open image: " + infile)
